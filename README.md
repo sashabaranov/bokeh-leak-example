5c632aad42ce35d13e6b60f88ec7cbb89ce937a2
Sasha Baranov <sashab1@yandex-team.ru>
2016-10-14 19:45:07 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # bokeh-leak-example
+
+Example to demonstrate memory leak when using Bokeh library.
+
+Howto:
+
+1. `pip install -r requirements.txt`
+2. `python app.py`
+3. Go to `http://127.0.0.1:5000/`
+4. Observe memory increase when clicking `Load plots` button
+
+
+All logic is in `app.py` and `index.html`
---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, jsonify
+from data import bins
+
+from bokeh.plotting import figure
+from bokeh.embed import components
+
+
+app = Flask(__name__, template_folder=".")
+
+@app.route("/")
+def hello():
+    return render_template("index.html")
+
+
+@app.route("/bokeh")
+def histos_for_path():
+    scripts, divs = "", ""
+
+    for _ in xrange(10):
+        plot = figure(
+            tools="pan,wheel_zoom,box_zoom,reset",
+            active_scroll="wheel_zoom",
+            toolbar_location="right"
+        )
+
+        plot.quad(
+           top=[b.top for b in bins],
+           bottom=[b.bottom for b in bins],
+           left=[b.left for b in bins],
+           right=[b.right for b in bins],
+           color=[b.color for b in bins],
+        )
+        script, div = components(plot, wrap_script=False)
+        scripts += script
+        divs += div
+
+
+    return jsonify({
+        "html": divs,
+        "script": scripts,
+    })
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
